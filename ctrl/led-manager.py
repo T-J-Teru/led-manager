7e70693d1bf366d46a1f6203bef12874c8ec5109
Andrew Burgess <andrew.burgess@embecosm.com>
2020-12-10 22:06:39 +0000

    Add command line argument processing, and a --config argument
    Allow the path to the config file to be passed on the command line.

--- ctrl/led-manager.py
+++ ctrl/led-manager.py
@@ -2,6 +2,7 @@ from mode_manager import network_mode_manager
 import time
 import os
 from led_manager_settings import led_manager_settings
+import argparse
 
 # COMMAND is a full command, including possible arguments.
 def make_lambda (command):
@@ -19,9 +20,21 @@ def build_items (config):
     return items
 
 def main ():
-    exe_dir = os.path.abspath (os.path.dirname (__file__))
-    config_path = os.path.join (exe_dir, "led-manager.conf")
+    # Process command line options.
+    parser = argparse.ArgumentParser(description='Control LED scripts.')
+    parser.add_argument('--config', help='config file to read', default=None)
+    args = parser.parse_args()
 
+    # Figure out the path to the config file, or look in the same
+    # directory as the script for a default.
+    config_path = args.config
+    if config_path == None:
+        exe_dir = os.path.abspath (os.path.dirname (__file__))
+        config_path = os.path.join (exe_dir, "led-manager.conf")
+    else:
+        config_path = os.path.abspath (config_path)
+
+    # Now start up the server.
     config = led_manager_settings (config_path)
     items = build_items (config)
     mm = network_mode_manager (config.port (), items, config.default ())
